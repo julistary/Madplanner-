7454b3dfd70c3ca0dba8f2afadd71a6a155767cf
“julistary” <julistary@gmail.com>
2021-05-12 17:53:28 +0200

    probando gp

---
+++ src/google_places.py
@@ -0,0 +1,55 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import src.foursquares as fs
+load_dotenv()
+
+key = os.getenv("key")
+
+url_query = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+what = "query="
+key_endpoint = "&key=" + key
+fields = "&fields=place_id, rating, name, geometry, formatted_address"
+pt = "pagetoken="
+
+
+def gp_call(query):
+    return requests.get(url= url_query+what+query+fields+key_endpoint).json()
+
+def next_page(next_page_token):
+    return requests.get(url = url_query + pt + next_page_token + key_endpoint).json()
+
+def request_to_df(query):
+    lista = []
+    data = query.get("results")
+    name = []
+    latitude = []
+    longitude = []
+    addres = []
+    rating = []
+
+    for d in data:
+        list_ = {}
+        list_["name"] = d.get("name")
+        list_["rating"] = d.get("rating")
+        list_["latitude"] = d.get("geometry").get("location").get("lat")
+        list_["longitude"] = d.get("geometry").get("location").get("lng")
+        list_["address"] = d.get("formatted_address")
+        lista.append(list_)
+
+    documentos = []
+    for diccionario in lista:
+        temporal = {
+            "name": diccionario.get("name"),
+            "rating" : diccionario.get("rating"),
+            "location": {"type": "Point", "coordinates": [diccionario.get("longitude"), diccionario.get("latitude")]},
+            "address" : diccionario.get("address")
+
+        }
+        documentos.append(temporal)
+
+
+    df = pd.DataFrame(documentos)
+    return df
